5427730bb97a414df1f463f67e532d3847e82391
Emily LeFebvre <Emily@Lefebvre.us>
2023-03-27 13:56:47 -0500

    fini

--- module-3/11_Intro_to_Vue_JS/exercise/src/components/UserList.vue
+++ module-3/11_Intro_to_Vue_JS/exercise/src/components/UserList.vue
@@ -7,45 +7,37 @@
         <th>Username</th>
         <th>Email Address</th>
         <th>Status</th>
-        <th>Actions</th>
     </tr>
     </thead>
     <tbody>
       <tr>
+        <td><input v-model="filter.firstName" type="text" id="firstNameFilter"/></td>
+        <td><input v-model="filter.lastName" type="text" id="lastNameFilter"/></td>
+        <td><input v-model="filter.username" type="text" id="usernameFilter"/></td>
+        <td><input v-model="filter.emailAddress" type="text" id="emailFilter"/></td>
         <td>
-          <input type="text" id="firstNameFilter" v-model="filter.firstName"/>
-        </td>
-        <td>
-          <input type="text" id="lastNameFilter"  v-model="filter.lastName"/>
-        </td>
-        <td>
-          <input type="text" id="usernameFilter"  v-model="filter.username"/>
-        </td>
-        <td>
-          <input type="text" id="emailFilter"  v-model="filter.emailAddress"/>
-        </td>
-        <td>
-          <select id="statusFilter" v-model="filter.status">
-            <option value>Show All</option>
+          <select v-model="filter.status" id="statusFilter">
+            <option value="">Show All</option>
             <option value="Active">Active</option>
             <option value="Disabled">Disabled</option>
+            <td><input v-model="filter.status" type="text"/></td>
           </select>
         </td>
       </tr>
-      <tr v-for="user in filteredList" v-bind:key="user.id" :class="{disabled: user.status === 'Disabled'}">
       <!-- user listing goes here -->
-        <td>{{ user.firstName }}</td>
-        <td>{{ user.lastName }}</td>
-        <td>{{ user.username }}</td>
-        <td>{{ user.emailAddress }}</td>
-        <td>{{ user.status }}</td>
-      </tr>
+      <tr v-for="user in filteredList"
+      v-bind:key="user.username"
+      v-bind:class="{disabled: user.status === 'Disabled'}"
+      >
+      <td>{{user.firstName}}</td>
+      <td>{{user.lastName}}</td>
+      <td>{{user.username}}</td>
+      <td>{{user.emailAddress}}</td>
+      <td>{{user.status}}</td>
+        </tr>
     </tbody>
-  </table> 
-
+  </table>
 </template>
-
-
 
 <script>
 export default {
@@ -53,108 +45,32 @@ export default {
   data() {
     return {
       filter: {
-        firstName: "",
-        lastName: "",
-        username: "",
-        emailAddress: "",
-        status: ""
+        firstName:"",
+        lastName:"",
+        username:"",
+        emailAddress:"",
+        status:""
       },
       users: [
-        {
-          //id: 1,
-          firstName: "John",
-          lastName: "Smith",
-          username: "jsmith",
-          emailAddress: "jsmith@gmail.com",
-          status: "Active"
-        },
-        {
-          //id: 2,
-          firstName: "Anna",
-          lastName: "Bell",
-          username: "abell",
-          emailAddress: "abell@yahoo.com",
-          status: "Active"
-        },
-        {
-          //id: 3,
-          firstName: "George",
-          lastName: "Best",
-          username: "gbest",
-          emailAddress: "gbest@gmail.com",
-          status: "Disabled"
-        },
-        {
-          //id: 4,
-          firstName: "Ben",
-          lastName: "Carter",
-          username: "bcarter",
-          emailAddress: "bcarter@gmail.com",
-          status: "Active"
-        },
-        {
-          //id: 5,
-          firstName: "Katie",
-          lastName: "Jackson",
-          username: "kjackson",
-          emailAddress: "kjackson@yahoo.com",
-          status: "Active"
-        },
-        {
-          //id: 6,
-          firstName: "Mark",
-          lastName: "Smith",
-          username: "msmith",
-          emailAddress: "msmith@foo.com",
-          status: "Disabled"
-        }
+        { firstName: 'John', lastName: 'Smith', username: 'jsmith', emailAddress: 'jsmith@gmail.com', status: 'Active' },
+        { firstName: 'Anna', lastName: 'Bell', username: 'abell', emailAddress: 'abell@yahoo.com', status: 'Active' },
+        { firstName: 'George', lastName: 'Best', username: 'gbest', emailAddress: 'gbest@gmail.com', status: 'Disabled' },
+        { firstName: 'Ben', lastName: 'Carter', username: 'bcarter', emailAddress: 'bcarter@gmail.com', status: 'Active' },
+        { firstName: 'Katie', lastName: 'Jackson', username: 'kjackson', emailAddress: 'kjackson@yahoo.com', status: 'Active' },
+        { firstName: 'Mark', lastName: 'Smith', username: 'msmith', emailAddress: 'msmith@foo.com', status: 'Disabled' }
       ]
-
     }
   },
-
-  computed: {
-  filteredList() {
-    let filteredUsers = this.users;
-    if (this.filter.firstName != "") {
-      filteredUsers = filteredUsers.filter((user) =>
-        user.firstName
-          .toLowerCase()
-          .includes(this.filter.firstName.toLowerCase())
-      );
+  computed:{
+    filteredList(){
+      return this.users.filter(user => user.firstName.toLowerCase().includes(this.filter.firstName) &&
+      user.lastName.toLowerCase().includes(this.filter.lastName) &&
+      user.username.includes(this.filter.username) &&
+      user.emailAddress.includes(this.filter.emailAddress) &&
+      user.status.includes(this.filter.status))
     }
-    if (this.filter.lastName != "") {
-      filteredUsers = filteredUsers.filter((user) =>
-        user.lastName
-          .toLowerCase()
-          .includes(this.filter.lastName.toLowerCase())
-      );
-    }
-    if (this.filter.username != "") {
-      filteredUsers = filteredUsers.filter((user) =>
-        user.username
-          .toLowerCase()
-          .includes(this.filter.username.toLowerCase())
-      );
-    }
-    if (this.filter.emailAddress != "") {
-      filteredUsers = filteredUsers.filter((user) =>
-        user.emailAddress
-          .toLowerCase()
-          .includes(this.filter.emailAddress.toLowerCase())
-      );
-    }
-    if (this.filter.status != "") {
-      filteredUsers = filteredUsers.filter((user) =>
-        user.status === this.filter.status
-      );
-    }
-    return filteredUsers;
   }
 }
-
-}
-
 </script>
 
 <style scoped>
@@ -174,4 +90,4 @@ tr.disabled {
 input, select {
   font-size: 16px;
 }
-</style> 
+</style>
