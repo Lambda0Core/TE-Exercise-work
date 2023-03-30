47b69a4c69ddf3697a8acb0369eab43a58fdc301
Emily LeFebvre <Emily@Lefebvre.us>
2023-03-30 09:44:27 -0500

    test

--- module-3/13_Vue_Component_Communication/exercise/src/components/ReadingList.vue
+++ module-3/13_Vue_Component_Communication/exercise/src/components/ReadingList.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="book-container">
-
+    <book-card v-for="book in $store.state.books" v-bind:book="book" v-bind:key="book.title" />
   </div>
 </template>
 
 <script>
+import BookCard from './BookCard.vue'
+
+//import component from ''
+
 
 export default {
-    name: 'reading-list'
+  components: { BookCard },
+  name: 'reading-list', 
 }
 </script>
 
 <style>
 .book-container {
-    display: flex;
+    display:flex;
     justify-content: space-evenly;
     flex-wrap: wrap;
 }
