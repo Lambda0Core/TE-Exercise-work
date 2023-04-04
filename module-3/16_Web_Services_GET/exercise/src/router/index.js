96fbc8a8256c128e8446b260f364dbafb48f48ad
Emily LeFebvre <Emily@lefebvre.us>
2023-04-04 07:47:42 -0700

    test

--- module-3/16_Web_Services_GET/exercise/src/router/index.js
+++ module-3/16_Web_Services_GET/exercise/src/router/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
-import Messages from '../views/Messages.vue';
+import Vue from 'vue'; // Access the code for Vue
+import VueRouter from 'vue-router'; // Access the code for Vue Router
+import Home from '@/views/Home.vue'; // Access the code for Home.vue
+import Messages from '@/views/Messages.vue'; // Access the code for Messages.vue
+import TopicDetails from '@/components/TopicDetails';
 
 Vue.use(VueRouter);
 
-const routes = [
+const routes = [ // Associate paths to components
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/topics/:id',
+    name: 'topic',
+    component: TopicDetails
   },
   {
     path: '/:id',
--- module-3/16_Web_Services_GET/exercise/src/views/Home.vue
+++ module-3/16_Web_Services_GET/exercise/src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="home">
-    <h1>Topic Message Board</h1>
+    <h1>Topic Message Board</h1> <!-- Component is referenced in kabob-case -->
     <topic-list />
   </div>
 </template>
 
 <script>
-import TopicList from '../components/TopicList.vue';
+import TopicList from '@/components/TopicList.vue';
 
 export default {
   name: 'Home',
